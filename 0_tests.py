bb2110c298a50ea7411a8e58c26a2f62a6dc0506
Rozalina Shahanska <129857786+ROSHAHDESIGN@users.noreply.github.com>
2024-11-13 09:22:21 +0200

    Add files via upload

---
+++ 0_tests.py
@@ -0,0 +1,15 @@
+#while True:
+   # pass
+
+# a = 5
+#
+# while a <= 10: #True OR False
+#     print("a =" + str(a))
+#     a += 1 #ako e -= 1 Vliza v bezkraen loop
+#
+# while True:
+#     print(f" ")
+#     condition1
+#     if ...:
+#         break #spira loop-a
+
---
+++ 1_read_text.py
@@ -0,0 +1,14 @@
+# text = input()
+
+# while text != "Stop":   #while True:
+#     print(text)         #if text == "Stop"
+#     text = input()      #break
+#                         #print(text)
+#                         #text = input()
+while True:
+    text = input()
+
+    if text == "Stop":
+        break
+
+    print(text)
